fb8f91754252252398a28ab7485c76cd824342ba
Wisnu Wicaksono <63142229+wiscaksono@users.noreply.github.com>
2023-08-18 07:58:18 +0700

    config dah paling bener ini

---
+++ mappings.lua
@@ -0,0 +1,23 @@
+-- Mapping data with "desc" stored directly by vim.keymap.set().
+--
+-- Please use this mappings table to set keyboard mapping since this is the
+-- lower level configuration and more robust one. (which-key will
+-- automatically pick-up stored data by this setting.)
+return {
+  -- first key is the mode
+  n = {
+    -- mappings seen under group name "Buffer"
+    ["<leader>bD"] = {
+      function()
+        require("astronvim.utils.status").heirline.buffer_picker(
+          function(bufnr) require("astronvim.utils.buffer").close(bufnr) end
+        )
+      end,
+      desc = "Pick to close",
+    },
+  },
+  t = {
+    -- setting a mapping to false will disable it
+    -- ["<esc>"] = false,
+  },
+}
---
+++ plugins/user.lua
@@ -0,0 +1,11 @@
+return {
+  -- You can also add new plugins here as well:
+  -- Add plugins, the lazy syntax
+  -- {
+  --   "ray-x/lsp_signature.nvim",
+  --   event = "BufRead",
+  --   config = function()
+  --     require("lsp_signature").setup()
+  --   end,
+  -- },
+}
