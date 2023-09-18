09753de9acd0e5eaa72b5ff4dbbd5b94b858c49b
Wisnu Wicaksono <wwicaksono96@gmail.com>
2023-09-18 08:49:33 +0700

    update community plugin

--- plugins/community.lua
+++ plugins/community.lua
@@ -1,42 +1,39 @@
 return {
   -- Add the community repository of plugin specifications
   "AstroNvim/astrocommunity",
-  -- example of importing a plugin, comment out to use it or add your own
-  -- available plugins can be found at https://github.com/AstroNvim/astrocommunity
   { import = "astrocommunity.pack.tailwindcss" },
   { import = "astrocommunity.pack.html-css" },
   { import = "astrocommunity.pack.json" },
-  { import = "astrocommunity.pack.rust" },
   { import = "astrocommunity.pack.prisma" },
-  { import = "astrocommunity.pack.cpp" },
-  { import = "astrocommunity.utility.transparent-nvim", enabled = true },
+  { import = "astrocommunity.pack.rust" },
+  -- { import = "astrocommunity.pack.cpp" },
+  { import = "astrocommunity.color.transparent-nvim", enabled = true },
   { import = "astrocommunity.motion.harpoon" },
   { import = "astrocommunity.media.presence-nvim" },
   { import = "astrocommunity.media.vim-wakatime" },
   {
     "presence.nvim",
     opts = {
-      auto_update = true, -- Update activity based on autocmd events (if `false`, map or manually execute `:lua package.loaded.presence:update()`)
-      neovim_image_text = "Vscode for noobs", -- Text displayed when hovered over the Neovim image
-      main_image = "file", -- Main image display (either "neovim" or "file")
-      client_id = "793271441293967371", -- Use your own Discord application client id (not recommended)
-      log_level = nil, -- Log messages at or above this level (one of the following: "debug", "info", "warn", "error")
-      debounce_timeout = 10, -- Number of seconds to debounce events (or calls to `:lua package.loaded.presence:update(<filename>, true)`)
-      enable_line_number = true, -- Displays the current line number instead of the current project
-      blacklist = {}, -- A list of strings or Lua patterns that disable Rich Presence if the current file name, path, or workspace matches
+      auto_update = true,
+      neovim_image_text = "Vscode for noobs",
+      main_image = "file",
+      client_id = "793271441293967371",
+      log_level = nil,
+      debounce_timeout = 10,
+      enable_line_number = true,
+      blacklist = {},
       buttons = {
         { label = "Please don't click!", url = "https://youtu.be/dQw4w9WgXcQ" },
-      }, -- Configure Rich Presence button(s), either a boolean to enable/disable, a static table (`{{ label = "<label>", url = "<url>" }, ...}`, or a function(buffer: string, repo_url: string|nil): table)
-      file_assets = {}, -- Custom file asset definitions keyed by file names and extensions (see default config at `lua/presence/file_assets.lua` for reference)
-      show_time = false, -- Show the timer
-      -- Rich Presence text options
-      editing_text = "Editing %s", -- Format string rendered when an editable file is loaded in the buffer (either string or function(filename: string): string)
-      file_explorer_text = "Browsing %s", -- Format string rendered when browsing a file explorer (either string or function(file_explorer_name: string): string)
-      git_commit_text = "Committing changes", -- Format string rendered when committing changes in git (either string or function(filename: string): string)
-      plugin_manager_text = "Managing plugins", -- Format string rendered when managing plugins (either string or function(plugin_manager_name: string): string)
-      reading_text = "Reading %s", -- Format string rendered when a read-only or unmodifiable file is loaded in the buffer (either string or function(filename: string): string)
-      workspace_text = "Working on %s", -- Format string rendered when in a git repository (either string or function(project_name: string|nil, filename: string): string)
-      line_number_text = "Line %s out of %s", -- Format string rendered when `enable_line_number` is set to true (either string or function(line_number: number, line_count: number): string)
+      },
+      file_assets = {},
+      show_time = false,
+      editing_text = "Editing %s",
+      file_explorer_text = "Browsing %s",
+      git_commit_text = "Committing changes",
+      plugin_manager_text = "Managing plugins",
+      reading_text = "Reading %s",
+      workspace_text = "Working on %s",
+      line_number_text = "Line %s out of %s",
     },
   },
   { import = "astrocommunity.completion.copilot-lua" },
