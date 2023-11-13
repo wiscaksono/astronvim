22cff545f889c7dce0dd9e265c9b742e73f064d7
Wisnu Wicaksono <wwicaksono96@gmail.com>
2023-11-13 19:19:49 +0700

    feat: add docker lsp

--- plugins/user.lua
+++ plugins/user.lua
@@ -1,12 +1,4 @@
 return {
   { "typicode/bg.nvim", lazy = false },
-  -- You can also add new plugins here as well:
-  -- Add plugins, the lazy syntax
-  -- {
-  --   "ray-x/lsp_signature.nvim",
-  --   event = "BufRead",
-  --   config = function()
-  --     require("lsp_signature").setup()
-  --   end,
-  -- },
+  { "Equilibris/nx.nvim", depends = { "nvim-telescope/telescope.nvim" }, opts = {} },
 }
