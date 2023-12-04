65e01d9409c950c65f199016e01ef49c2be7f007
Wisnu Wicaksono <wisnu.wicaksono@msiglife.com>
2023-12-04 17:09:25 +0700

    feat: add minor changes

--- plugins/user.lua
+++ plugins/user.lua
@@ -1,4 +1,10 @@
 return {
   { "typicode/bg.nvim", lazy = false },
   { "Equilibris/nx.nvim", depends = { "nvim-telescope/telescope.nvim" }, opts = {} },
+  {
+    "vuki656/package-info.nvim",
+    ft = "json",
+    dependencies = "MunifTanjim/nui.nvim",
+    opts = {},
+  },
 }
