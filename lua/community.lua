b7a303b0b6f8197798f4a3d8eea818b5fa1644ef
Wiscaksono <wwicaksono96@gmail.com>
2024-10-21 18:20:13 +0700

    cleanup

--- lua/community.lua
+++ lua/community.lua
@@ -13,18 +13,18 @@ return {
   { import = "astrocommunity.pack.go" },
   { import = "astrocommunity.pack.tailwindcss" },
   { import = "astrocommunity.pack.astro" },
-  { import = "astrocommunity.pack.templ" },
-  { import = "astrocommunity.pack.rust" },
-  { import = "astrocommunity.pack.elixir-phoenix" },
+  -- { import = "astrocommunity.pack.templ" },
+  -- { import = "astrocommunity.pack.rust" },
+  -- { import = "astrocommunity.pack.elixir-phoenix" },
   { import = "astrocommunity.pack.json" },
   { import = "astrocommunity.pack.prisma" },
-  { import = "astrocommunity.pack.vue" },
-  { import = "astrocommunity.pack.svelte" },
-  { import = "astrocommunity.pack.zig" },
+  -- { import = "astrocommunity.pack.vue" },
+  -- { import = "astrocommunity.pack.svelte" },
+  -- { import = "astrocommunity.pack.zig" },
   { import = "astrocommunity.pack.markdown" },
-  { import = "astrocommunity.pack.mdx" },
-  { import = "astrocommunity.pack.php" },
-  { import = "astrocommunity.pack.python" },
+  -- { import = "astrocommunity.pack.mdx" },
+  -- { import = "astrocommunity.pack.php" },
+  -- { import = "astrocommunity.pack.python" },
   -- COLORSCHEME
   { import = "astrocommunity.colorscheme.oldworld-nvim" },
   -- EDITING SUPPORT
@@ -32,40 +32,9 @@ return {
   -- LSP
   { import = "astrocommunity.lsp.nvim-java" },
   { import = "astrocommunity.lsp.actions-preview-nvim" },
-  -- { import = "astrocommunity.lsp.ts-error-translator-nvim" },
   -- MISC
   { import = "astrocommunity.completion.copilot-lua" },
   -- { import = "astrocommunity.completion.avante-nvim" },
   { import = "astrocommunity.media.vim-wakatime" },
-  { import = "astrocommunity.recipes.auto-session-restore" },
-  -- {
-  --   "AstroNvim/astrolsp",
-  --   opts = {
-  --     config = {
-  --       jdtls = {
-  --         settings = {
-  --           java = {
-  --             configuration = {
-  --               runtimes = {
-  --                 {
-  --                   name = "JavaSE-21",
-  --                   path = "/Users/wiscaksono/.sdkman/candidates/java/21.0.3-tem",
-  --                 },
-  --                 {
-  --                   name = "JavaSE-17",
-  --                   path = "/Users/wiscaksono/.sdkman/candidates/java/17.0.11-tem",
-  --                   default = true,
-  --                 },
-  --                 {
-  --                   name = "JavaSE-11",
-  --                   path = "/Users/wiscaksono/.sdkman/candidates/java/11.0.23-tem",
-  --                 },
-  --               },
-  --             },
-  --           },
-  --         },
-  --       },
-  --     },
-  --   },
-  -- },
+  -- { import = "astrocommunity.recipes.auto-session-restore" },
 }
