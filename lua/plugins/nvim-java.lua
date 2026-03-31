ddb60a75e7040ab8cc42f33e303b3ea69c921733
Wiscaksono <wwicaksono96@gmail.com>
2026-03-31 15:13:06 +0700

    chore: update plugin versions in lazy-lock.json and enhance nvim-java configuration

--- lua/plugins/nvim-java.lua
+++ lua/plugins/nvim-java.lua
@@ -1,6 +1,22 @@
 return {
   "nvim-java/nvim-java",
+  ft = "java",
   opts = {
     java_debug_adapter = { enable = false },
+    jdk = { auto_install = false },
+  },
+  specs = {
+    {
+      "AstroNvim/astrolsp",
+      optional = true,
+      opts = {
+        handlers = {
+          jdtls = function(server)
+            require("lazy").load({ plugins = { "nvim-java" } })
+            vim.lsp.enable(server)
+          end,
+        },
+      },
+    },
   },
 }
